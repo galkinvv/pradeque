b4673b4dfdc27c1dfaab4d2903e667c0efa275b1
galkinvv <galkin-vv@yandex.ru>
2014-10-19 20:33:10 +0400

    begin schema for aligned_uninitialized_array

--- pradeque.hpp
+++ pradeque.hpp
@@ -1,9 +1,61 @@
+#pragma once
+#include <numeric_limits>
+#include <cstdint>
+
 namespace pradeque_impl
 {
-template <class T, class TSize, int t_log2_max_size>
-struct core
+using namespace std;
+
+constexpr inline int GetUpperLog2(uint32_t i)
 {
-    template <class... Args>
+   //use macro-based oneliner
+   return 4;
+}
+template <class T, uint32_t tRequiredSize>
+class aligned_uninitialized_array_core
+{
+    struct statics
+	{
+		static const int kByteSizeUpperLog2 =GetUpperLog2(uint32_t(sizeof(T)) * tRequiredSize);
+		static const int kByteSize = 1 << kByteSizeUpperLog2;
+		static const int kItemSize = kByteSize/sizeof(T);
+	};
+
+  public:
+	template <class aligned_uninitialized_array_core>
+	class aligned_array_iterator
+	{
+		//single-pointer iterator that allows both deferencing and getting pointer to container (using alignment)
+		aligned_uninitialized_array_core& container()
+		{
+
+		}
+	};
+	iterator begin()
+	{
+
+	};
+	iterator end()
+	{
+
+	};
+};
+
+template <class T, class TSize, int t_log2_max_size, int t_log2_block_ratio>
+class core
+{
+	struct  statics
+	{
+		static constexpr TSize nth_array_elem_count(int positive_array_index)
+		{
+
+		}
+	};
+	//table of pointers to different-sized array must be aligmed in a way
+	//allowing determine both pointer to table start and table index from single pointer.
+public:
+
+	template <class... Args>
     void emplace_front(Args&&... args)
     {
         //emplace_* methods are only ones having complex exception safety semantics

863ef7e6dc45d5ba6e32fdb9cbdad52101b26f70
galkinvv <galkin-vv@yandex.ru>
2015-03-04 10:38:08 +0300

    constants via enumeration values

--- pradeque-inl.h
+++ pradeque-inl.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <stdint.h>
 #include <assert.h>
+#include <limits.h>
 
 #define PRA_DEQUE_DETAIL_EMPTY
 #define PRA_DEQUE_DETAIL_DEFER(id) id PRA_DEQUE_DETAIL_EMPTY
@@ -54,9 +55,41 @@ static inline int praDequeDetail_LowerLog2(uint32_t i)
    return PRA_DEQUE_DETAIL_LOWER_LOG2_UINT32(i);
 }
 
+
+
+enum{
+kPradequeUnusedPPVoidBits = PRA_DEQUE_DETAIL_LOWER_LOG2_UINT32(sizeof(void*)),
+ kPradequeAddressBits = sizeof(void*) * CHAR_BIT,
+
+//count of bits required to adress element in a table of block entries
+//the value should be smaller to minimize memory overhead for table allocation, which is done with quite small number of elements
+//the value should be greatear to allow more effective buffer usage by allowing more buffer sizes
+//the selected tecnbhique is to use 3 buffers of every size with sizes exponentially growing by 4
+//because verry big and very smal containers are not used 32 table eentryes are enough to store one exponential sequence of blocks. It must be doubled for allowing both back and front exponential growth.
+//so 6 bit is enough for 32 bit case.
+//for doubling/halfing adddress space with the formula adds/subtracts a bit. This give nice result for current 64-bit systems (allowing up to 2^48 sizes) and meaningful result for 16 and 128 bit systems, so it looks good.
+
+ kPradequeTableAddressBits = PRA_DEQUE_DETAIL_LOWER_LOG2_UINT32(kPradequeAddressBits)  + 1,
+kPradequeSizeBits = kPradequeAddressBits - (kPradequeTableAddressBits - kPradequeUnusedPPVoidBits),
+
+kPradequeHalfTableSize = 1 << (kPradequeTableAddressBits  - 1),
+kPradequeHalfTableSmallSpecialEntrys = 2, //count of first element that does not correspond to standard size calculating formula.
+//table size defines the relation between smallest and biggesst arrays.
+
+};
+
 #ifndef PRA_DEQUE_DETAIL_API
 #define PRA_DEQUE_DETAIL_API inline
 #endif
 
 PRA_DEQUE_DETAIL_API
 void pradeque_release(pradeque_t* deque, pradeque_params_t* params){}
+
+PRA_DEQUE_DETAIL_API
+intptr_t pradeque_max_size(pradeque_params_t* params)
+{
+    //size is limited by two factors: maximal allocatable array and number of bits used for size storage
+    //maximal allocatable array is limited by address space and table size
+	return 0;
+}
+

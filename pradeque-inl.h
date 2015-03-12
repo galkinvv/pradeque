6a421b21b4f59af5732b4ddd10616908337da62e
Vasily Galkin <galkin-vv@yandex.ru>
2015-03-12 15:14:08 +0400

    inl

--- pradeque-inl.h
+++ pradeque-inl.h
@@ -19,10 +19,10 @@
 #define PRA_DEQUE_DETAIL_LOWER_LOG2_UINT32(x) (PRA_DEQUE_DETAIL_EXPAND(PRA_DEQUE_DETAIL_EXPAND(PRA_DEQUE_DETAIL_EXPAND(PRA_DEQUE_DETAIL_EXPAND(PRA_DEQUE_DETAIL_LOWER_LOG2_UINT_HELPER32(((uint32_t)(x))))))))
 #define PRA_DEQUE_DETAIL_LOWER_LOG2_UINT64(x) (PRA_DEQUE_DETAIL_EXPAND(PRA_DEQUE_DETAIL_EXPAND(PRA_DEQUE_DETAIL_EXPAND(PRA_DEQUE_DETAIL_EXPAND(PRA_DEQUE_DETAIL_EXPAND(PRA_DEQUE_DETAIL_LOWER_LOG2_UINT_HELPER64(((uint64_t)(x)))))))))
 
-enum{
+enum PradequeDetailConstants{
 
-kPradequeUnusedPPVoidBits = PRA_DEQUE_DETAIL_LOWER_LOG2_UINT32(sizeof(void*)),
-kPradequeAddressBits = sizeof(void*) * CHAR_BIT,
+kPradequeDetailUnusedPPVoidBits = PRA_DEQUE_DETAIL_LOWER_LOG2_UINT32(sizeof(void*)),
+kPradequeDetailAddressBits = sizeof(void*) * CHAR_BIT,
 
 //count of bits required to adress element in a table of block entries
 //the value should be smaller to minimize memory overhead for table allocation, which is done with quite small number of elements
@@ -32,21 +32,21 @@ kPradequeAddressBits = sizeof(void*) * CHAR_BIT,
 //so 6 bit is enough for 32 bit case.
 //for doubling/halfing adddress space with the formula adds/subtracts a bit. This give nice result for current 64-bit systems (allowing up to 2^48 sizes) and meaningful result for 16 and 128 bit systems, so it looks good.
 
-kPradequeTableIndexBits = PRA_DEQUE_DETAIL_LOWER_LOG2_UINT32(kPradequeAddressBits) + 1,
+kPradequeDetailTableIndexBits = PRA_DEQUE_DETAIL_LOWER_LOG2_UINT32(kPradequeDetailAddressBits) + 1,
 
 //limitation to size bits coming from storage scheme: pradeque_t stores in two intptr_t packed fields pointer to last table entry, first table entry index and size.
-kPradequeSizeBits = 2 * kPradequeAddressBits - (kPradequeAddressBits - kPradequeUnusedPPVoidBits) - kPradequeTableIndexBits,
+kPradequeDetailSizeBits = 2 * kPradequeDetailAddressBits - (kPradequeDetailAddressBits - kPradequeDetailUnusedPPVoidBits) - kPradequeDetailTableIndexBits,
 
-kPradequeHalfTableSize = 1 << (kPradequeTableIndexBits - 1),
-kPradequeHalfTableSmallSpecialEntries = 2, //count of first element that does not correspond to standard size calculating formula.
-kPradequeHalfTableSmallSpecial0MinSize = sizeof(void*)*2, //min object count of special entry 0. In first version to simplicity is much enouhg to place pointer in half of block
-kPradequeHalfTableSmallSpecial1MinSize = 3*kPradequeHalfTableSmallSpecial0MinSize, //min size of special entry 1
-kPradequeHalfTableSmallSpecialTotalMinBits = PRA_DEQUE_DETAIL_LOWER_LOG2_UINT64(kPradequeHalfTableSmallSpecial0MinSize + kPradequeHalfTableSmallSpecial1MinSize), //min bitness of total elements in all small entries
-kPradequeFirstAllocationMaxOptimalBytes = 512, //byte count being optimal for first allocation (size for gcc std deque, also leads to minimal block size 32 for 64-bit objects)
+kPradequeDetailHalfTableSize = 1 << (kPradequeDetailTableIndexBits - 1),
+kPradequeDetailHalfTableSmallSpecialEntries = 2, //count of first element that does not correspond to standard size calculating formula.
+kPradequeDetailHalfTableSmallSpecial0MinSize = sizeof(void*)*2, //min object count of special entry 0. In first version to simplicity is much enouhg to place pointer in half of block
+kPradequeDetailHalfTableSmallSpecial1MinSize = 3*kPradequeDetailHalfTableSmallSpecial0MinSize, //min size of special entry 1
+kPradequeDetailHalfTableSmallSpecialTotalMinBits = PRA_DEQUE_DETAIL_LOWER_LOG2_UINT64(kPradequeDetailHalfTableSmallSpecial0MinSize + kPradequeDetailHalfTableSmallSpecial1MinSize), //min bitness of total elements in all small entries
+kPradequeDetailFirstAllocationMaxOptimalBytes = 512, //byte count being optimal for first allocation (size for gcc std deque, also leads to minimal block size 32 for 64-bit objects)
 
 //table size defines the relation between smallest and biggesst arrays.
-kPradequeHalfTableGroupedBy3Entries = (kPradequeHalfTableSize - kPradequeHalfTableSmallSpecialEntries) / 3,
-kPradequeLog2OfCapacityGrowByAddingAllGroupedBy3 = 2 * kPradequeHalfTableGroupedBy3Entries,
+kPradequeDetailHalfTableGroupedBy3Entries = (kPradequeDetailHalfTableSize - kPradequeDetailHalfTableSmallSpecialEntries) / 3,
+kPradequeDetailLog2OfCapacityGrowByAddingAllGroupedBy3 = 2 * kPradequeDetailHalfTableGroupedBy3Entries
 };
 
 //utility functions
@@ -88,20 +88,20 @@ static inline uintptr_t praDequeDetail_Packed(uintptr_t main_being_multiple, uin
 //return bit limitation of maximal size calculating maximal how much objects of size rounded up to next power of two can be placed in half of address space
 static inline int praDequeDetail_MaxSizeBitsUpToDeg2InHalfAddressSpace(size_t value_size)
 {
-    return kPradequeAddressBits - 1 - praDequeDetail_UpperLog2(value_size);
+    return kPradequeDetailAddressBits - 1 - praDequeDetail_UpperLog2(value_size);
 }
 
 //Calculates shift to use for calculation value_size specific small block sizes from default small block size.
 static inline int praDequeDetail_ShiftForSmallBlockSizes(size_t value_size)
 {
     //*2 corresponds to single allocation of two smallest blocks.
-    const int small_blocks_in_optimal_bytes = kPradequeFirstAllocationMaxOptimalBytes / (value_size * kPradequeHalfTableSmallSpecial0MinSize * 2);
+    const int small_blocks_in_optimal_bytes = kPradequeDetailFirstAllocationMaxOptimalBytes / (value_size * kPradequeDetailHalfTableSmallSpecial0MinSize * 2);
     return praDequeDetail_LowerLog2(small_blocks_in_optimal_bytes);
 }
 //Calculates bits required to adress value in all small block.
 static inline int praDequeDetail_TotalHalfSmallBlocksAdressBits(size_t value_size)
 {
-    return kPradequeHalfTableSmallSpecialTotalMinBits + praDequeDetail_ShiftForSmallBlockSizes(value_size);
+    return kPradequeDetailHalfTableSmallSpecialTotalMinBits + praDequeDetail_ShiftForSmallBlockSizes(value_size);
 }
 //API implementation
 #ifndef PRA_DEQUE_DETAIL_API
@@ -114,11 +114,12 @@ void pradeque_clear(pradeque_t* deque, pradeque_params_t* params){}
 PRA_DEQUE_DETAIL_API
 intptr_t pradeque_max_size(size_t value_size)
 {
-    const int max_bits_size = kPradequeSizeBits;//bits used for size storage
+    const int max_bits_size = kPradequeDetailSizeBits;//bits used for size storage
     const int max_bits_heap = praDequeDetail_MaxSizeBitsUpToDeg2InHalfAddressSpace(value_size);//maximal allocatable array is limited by address space
-    const int max_bits_table = praDequeDetail_TotalHalfSmallBlocksAdressBits(value_size) + kPradequeLog2OfCapacityGrowByAddingAllGroupedBy3 + 1;//log2 of element count that can be placed in all blocks in table. +1 corresponds to table consisting of two halves.
+    const int max_bits_table = praDequeDetail_TotalHalfSmallBlocksAdressBits(value_size) + kPradequeDetailLog2OfCapacityGrowByAddingAllGroupedBy3 + 1;//log2 of element count that can be placed in all blocks in table. +1 corresponds to table consisting of two halves.
 	
 	//first calculate max size bitness, then calculate size itself
-	return 0;
+    const int max_heap_table = max_bits_heap > max_bits_table ? max_bits_heap : max_bits_table;
+    const int max_all = max_bits_size > max_heap_table ? max_bits_size : max_heap_table;
+	return (((intptr_t)1) << max_all) - 1;
 }
-

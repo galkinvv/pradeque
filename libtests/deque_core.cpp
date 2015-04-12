7b9fc03f4e08436375f78f1845716e0647dd7a54
galkinvv <galkin-vv@yandex.ru>
2015-04-13 00:06:48 +0300

    zero count via macro working and passing utests

--- libtests/deque_core.cpp
+++ libtests/deque_core.cpp
@@ -36,7 +36,8 @@ TEST(deque_core, plain_c_api)
 template <uint64_t tValue>
 struct Log2
 {
-	static int LowerLog(){return PRA_DEQUE_LOWER_LOG2_UINT64_DETAILV1(tValue);}
+	static const int kLowerLog = PRA_DEQUE_LOWER_LOG2_UINT64_DETAILV1(tValue);
+	static int LowerLog(){return kLowerLog;}
 	static int UpperLog(){return praDequeDetail_UpperLog2(tValue);}
 };
 
@@ -83,14 +84,58 @@ TEST(log2, macro_64_direct_big_values)
 	EXPECT_EQ(PRA_DEQUE_LOWER_LOG2_UINT64_DETAILV1(0xFFFFFFF), 7*4-1);
 	EXPECT_EQ(PRA_DEQUE_LOWER_LOG2_UINT64_DETAILV1(0x8000000), 7*4-1);
 	EXPECT_EQ(PRA_DEQUE_LOWER_LOG2_UINT64_DETAILV1(0x80000000ull), 8*4-1);
-	EXPECT_EQ(PRA_DEQUE_LOWER_LOG2_UINT64_DETAILV1(0x7FFFFFFFFFFFFFFF), 16*4-2);
+	EXPECT_EQ(PRA_DEQUE_LOWER_LOG2_UINT64_DETAILV1(0x7FFFFFFFFFFFFFFFll), 16*4-2);
 	EXPECT_EQ(PRA_DEQUE_LOWER_LOG2_UINT64_DETAILV1(0x8000000000000000ull), 16*4-1);
-	EXPECT_EQ(PRA_DEQUE_LOWER_LOG2_UINT64_DETAILV1(0xFFFFFFFFFFFFFFFF), 16*4-1);
+	EXPECT_EQ(PRA_DEQUE_LOWER_LOG2_UINT64_DETAILV1(0xFFFFFFFFFFFFFFFFll), 16*4-1);
 }
 
 TEST(log2, macro_64_direct_operation_priority)
 {
 	EXPECT_EQ(PRA_DEQUE_LOWER_LOG2_UINT64_DETAILV1(2000 ? 7 : 2000), 2);
 	EXPECT_EQ(-PRA_DEQUE_LOWER_LOG2_UINT64_DETAILV1(0x80), -7);
+}
+
+template <uint64_t tValue>
+struct ZCount
+{
+	static const int kZeroes = PRA_DEQUE_LOW_ZEROES_UINT64_DETAILV1(tValue);
+	static int Zeroes(){return kZeroes;}
+};
+
+TEST(zero_count, func_via_class)
+{
+    EXPECT_EQ(ZCount<  1>::Zeroes(), 0);
+    EXPECT_EQ(ZCount<  2>::Zeroes(), 1);
+    EXPECT_EQ(ZCount<  3>::Zeroes(), 0);
+    EXPECT_EQ(ZCount<  4>::Zeroes(), 2);
+    EXPECT_EQ(ZCount<  5>::Zeroes(), 0);
+    EXPECT_EQ(ZCount<  6>::Zeroes(), 1);
+    EXPECT_EQ(ZCount<  7>::Zeroes(), 0);
+    EXPECT_EQ(ZCount<  8>::Zeroes(), 3);
+    EXPECT_EQ(ZCount<  9>::Zeroes(), 0);
+    EXPECT_EQ(ZCount<0xA>::Zeroes(), 1);
+    EXPECT_EQ(ZCount<0xB>::Zeroes(), 0);
+    EXPECT_EQ(ZCount<0xC>::Zeroes(), 2);
+    EXPECT_EQ(ZCount<0xD>::Zeroes(), 0);
+    EXPECT_EQ(ZCount<0xE>::Zeroes(), 1);
+    EXPECT_EQ(ZCount<0xF>::Zeroes(), 0);
+    EXPECT_EQ(ZCount<16>::Zeroes(), 4);
+    EXPECT_EQ(ZCount<17>::Zeroes(), 0);
+}
+
+TEST(zero_count, macro_64_direct_big_values)
+{
+	EXPECT_EQ(PRA_DEQUE_LOW_ZEROES_UINT64_DETAILV1(0xFFFFFFF), 0);
+	EXPECT_EQ(PRA_DEQUE_LOW_ZEROES_UINT64_DETAILV1(0x8000000), 6*4+3);
+	EXPECT_EQ(PRA_DEQUE_LOW_ZEROES_UINT64_DETAILV1(0x80000000ull), 8*4-1);
+	EXPECT_EQ(PRA_DEQUE_LOW_ZEROES_UINT64_DETAILV1(0x7FFFFFFFFFFFFFFFll), 0);
+	EXPECT_EQ(PRA_DEQUE_LOW_ZEROES_UINT64_DETAILV1(0x8000000000000000ull), 16*4-1);
+	EXPECT_EQ(PRA_DEQUE_LOW_ZEROES_UINT64_DETAILV1(0xFFFFFFFFFFFFFFFFll), 0);
+}
+
+TEST(zero_count, macro_64_direct_operation_priority)
+{
+	EXPECT_EQ(PRA_DEQUE_LOW_ZEROES_UINT64_DETAILV1(2000 ? 128*3 : 2000), 7);
+	EXPECT_EQ(-PRA_DEQUE_LOW_ZEROES_UINT64_DETAILV1(0x80), -7);
 }
 
